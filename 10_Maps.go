a27e4fc3fe570da002d77b00d512347581820cbb
Jihye Lee <30456239+JiSWU@users.noreply.github.com>
2019-01-07 20:06:23 +0900

    Create 10_Maps.go

---
+++ 10_Maps.go
@@ -0,0 +1,27 @@
+package main
+import "fmt"
+
+func main() {
+
+    m := make(map[string]int)
+
+    m["k1"] = 7
+    m["k2"] = 13
+
+    fmt.Println("map:", m)
+
+    v1 := m["k1"]
+    fmt.Println("v1: ", v1)
+
+    fmt.Println("len:", len(m))
+
+    delete(m, "k2")
+    fmt.Println("map:", m)
+
+    //2번째 인자는 존재 여부, 값이 0인지 없는건지 확인 가능
+    _, prs := m["k2"]
+    fmt.Println("prs:", prs)
+
+    n := map[string]int{"foo": 1, "bar": 2}
+    fmt.Println("map:", n)
+}
